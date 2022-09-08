0f220e81301fc5ca956b63655f7136561a33ad02
ROHIT BISWAS <79495712+rohitproject123@users.noreply.github.com>
2022-09-08 21:13:06 +0530

    Add files via upload

---
+++ 8.9 lab session/Main.java
@@ -0,0 +1,16 @@
+class A{
+	
+}
+public class Main {
+
+	
+		// TODO Auto-generated method stub
+        Main(){
+        	A ob =new A();
+        	
+        }
+        public static void main(String[] args) {
+        	Main obj = new Main();
+        }
+	}
+
---
+++ 8.9 lab session/Question2.java
@@ -0,0 +1,14 @@
+//Q.3... Develop a public java class and make sure nobody can create 
+//any object of this class from outside the class
+
+
+public class Question2 {
+     private Question2() {
+    	 
+     }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+
+}  }
